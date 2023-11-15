c156e25031684b7b8bd2967e473e7a4148b1bdd3
KingYen <1696918097@qq.com>
2023-11-15 18:28:50 +0800

    Git init.

---
+++ src/main/java/com/qitsoftwarestudio/usercenterrealend/UserCenterRealendApplication.java
@@ -0,0 +1,13 @@
+package com.qitsoftwarestudio.usercenterrealend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCenterRealendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterRealendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qitsoftwarestudio/usercenterrealend/UserCenterRealendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qitsoftwarestudio.usercenterrealend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCenterRealendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
